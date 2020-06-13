6731a479ca3e4fe3c4bd7f475e62f557e30b4a37
April Ma <54913304+april5622@users.noreply.github.com>
2020-06-12 20:51:22 -0700

    new save

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 
 export class TodoItem extends Component {
+
     getStyle = () => {
         return {
             background: '#f4f4f4',
@@ -11,8 +12,7 @@ export class TodoItem extends Component {
             textDecoration: this.props.todo.completed ? 
             'line-through' : 'none'
         }
-    }
-
+    };
 
     render() {
         const { id, title } = this.props.todo;
@@ -20,14 +20,14 @@ export class TodoItem extends Component {
             <div style = {this.getStyle()}>
                 <p>
                     <input type='checkbox' 
-                    onChange={this.props.markComplete.bind(this, id)}
+                        onChange={this.props.markComplete.bind(this, id)}
                     /> {' '}
-                    {title}
+                        {title}
                     <button 
                         onClick={this.props.delTodo.bind(this, id)} 
                         style={btnStyle}>
-                            x
-                        </button>
+                        x
+                    </button>
                 </p>
             </div>
         )
